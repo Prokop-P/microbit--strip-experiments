332718fe72d0089ab9609f60077f93165e97e566
Prokop-P <pospisil.prokop@gmail.com>
2024-12-06 20:45:32 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,34 @@
-let Led = 0
-let strip = neopixel.create(DigitalPin.P1, 6, NeoPixelMode.RGB)
-strip.setBrightness(150)
-basic.forever(function () {
-    for (let index = 0; index < 6; index++) {
-        strip.shift(1)
-        strip.setPixelColor(Led, neopixel.colors(NeoPixelColors.Yellow))
-        strip.show()
-        basic.pause(200)
-        Led += 1
-    }
-    Led = 0
+input.onButtonPressed(Button.A, function () {
+    had()
 })
+function had () {
+    while (true) {
+        for (let index = 0; index < 6; index++) {
+            strip.shift(1)
+            strip.setPixelColor(Led, neopixel.colors(NeoPixelColors.Red))
+            strip.show()
+            basic.pause(200)
+            Led += 1
+        }
+        Led = 0
+        for (let index = 0; index < 6; index++) {
+            strip.shift(1)
+            strip.setPixelColor(Led, neopixel.colors(NeoPixelColors.Green))
+            strip.show()
+            basic.pause(200)
+            Led += 1
+        }
+        Led = 0
+        for (let index = 0; index < 6; index++) {
+            strip.shift(1)
+            strip.setPixelColor(Led, neopixel.colors(NeoPixelColors.Blue))
+            strip.show()
+            basic.pause(200)
+            Led += 1
+        }
+        Led = 0
+    }
+}
+let Led = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 6, NeoPixelMode.RGB)
